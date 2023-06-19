804f086e5c1b51165c4b5bfa5eabcc17aafb3784
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-06-19 23:10:17 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clean_folder',
+      version='1.0.0',
+      description='Package for sorting folders',
+      url='http://github.com/dummy_user/useful',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['useful'])
