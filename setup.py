6dbe1163b942ac149906ed0b8b98c8decb97df77
Dishalex <131618968+Dishalex@users.noreply.github.com>
2023-06-19 23:53:53 +0300

    update package

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-setup(name='clean_folder',
-      version='1.0.0',
+setup(name='Clean folder',
+      version='0.0.1',
       description='Package for sorting folders',
-      url='http://github.com/dummy_user/useful',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      url='https://github.com/Dishalex/Clean_folder',
+      author='Oleksandr Dyshliuk',
+      author_email='dishalex@gmail.com',
       license='MIT',
-      packages=['useful'])
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
+      packages=['clean_folder'],
+      include_package_data=True,
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean_my_folder=clean_folder.clean:main']})
